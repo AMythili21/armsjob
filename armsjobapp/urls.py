e92a7974984f356d9c0149d0018bf9df9f4fde8f
AMythili21 <mythili@psdigitise.com>
2025-05-03 12:04:35 +0530

    app url updated

---
+++ armsjobapp/urls.py
@@ -0,0 +1,23 @@
+from django.urls import path
+from .views import AgentSupplierListCreateView,AgentSupplierUpdateView,AgentSupplierDeleteView
+from .views import CandidateListCreateView, CandidateUpdateView, CandidateDeleteView,CandidateNameListView,CandidateDetailView,CandidateRemarksCreateView
+
+
+from django.http import JsonResponse
+
+def home(request):
+    return JsonResponse({"message": "Django is working on Vercel!"})
+
+urlpatterns = [
+    path('', home),  # Handles the root path
+    path('agents/',AgentSupplierListCreateView.as_view()),
+    path('agents/update/', AgentSupplierUpdateView.as_view(), name='agent-supplier-update'),
+    path('agents/delete/', AgentSupplierDeleteView.as_view(), name='agent-supplier-delete'),
+    path('candidates/', CandidateListCreateView.as_view(), name='candidate-list-create'),
+    path('candidates/update/<int:candidate_id>/', CandidateUpdateView.as_view(), name='candidate-update'),
+    path('candidates/delete/<int:candidate_id>/', CandidateDeleteView.as_view(), name='candidate-delete'),
+    path('candidates/names/', CandidateNameListView.as_view(), name='candidate-name-list'),
+    path('candidates/<int:candidate_id>/', CandidateDetailView.as_view(), name='candidate-detail'),
+    path('remarks/create/', CandidateRemarksCreateView.as_view(), name='candidate-remark-create'),
+
+]
